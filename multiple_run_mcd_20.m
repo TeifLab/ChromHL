dafdf2e3178bb2522a235a8ab1e0ec51057205b1
Graeme Thorn <graeme.thorn@gmail.com>
2018-05-02 16:58:45 +0100

    Add files by upload - corrected

--- multiple_run_mcd_20.m
+++ multiple_run_mcd_20.m
@@ -12,8 +12,8 @@ end
 try
     system(['mkdir -p Text_output/']);
     for kk=((job_id-1)*100):((job_id*100)-1)
-        identifier=pad(num2str(kk),4,'left','0');
-        Seq_DNA=Read_FASTA_all(['/home/gthorn/teif/graeme/Intersect_Jenuwein_MCORE_peaks/WT_not_DKO/WT_not_DKO_peaks/FASTA/region_WT_not_DKO_' identifier '*.fa']);
+        identifier=pad(num2str(kk),3,'left','0');
+        Seq_DNA=Read_FASTA_all(['../individual_regions/gene_region_' identifier '*.fa']);
         if ~isempty(Seq_DNA)
             Driver;
             system(['mv output-' zz '.mat output_' sprintf('%04d',kk) '.mat']);
