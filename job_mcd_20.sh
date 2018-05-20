16d368134ca7a53f704c41e01217b4c5d5de2996
Graeme Thorn <graeme.thorn@gmail.com>
2018-05-20 17:19:35 +0100

    Include comments and tidy up shell script

--- job_mcd_20.sh
+++ job_mcd_20.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 
-# SGE job script for sumbitting matlab jobs to an SGE cluster queue.
-# This is submitted to the queue by the job.sh script. It simply runs
-#  matlab with the correct arguments.
-# By David Black-Schaffer, June 2007.
-# Permission to use and modify this script is granted.
-# I am not responsible for any errors in this script, so be forewarned!
+# This script is for submitting multiple runs to the an SGE cluster queue
+# in MATLAB.
+#
+# Alter the -m for notification options
+#           -M for email notification
+#           -t for number of subtasks (passed as env variable SGE_TASK_ID)
+#           -tc for number of concurrent tasks to run
+#
+# 
+# Original header below:
+#
+## SGE job script for sumbitting matlab jobs to an SGE cluster queue.
+## This is submitted to the queue by the job.sh script. It simply runs
+##  matlab with the correct arguments.
+## By David Black-Schaffer, June 2007.
+## Permission to use and modify this script is granted.
+## I am not responsible for any errors in this script, so be forewarned!
 
 
 #$ -j n
@@ -19,9 +30,19 @@
 
 echo "Starting job: $SGE_TASK_ID"
 
-# Modify this to use the path to matlab for your system
+# Run matlab without display, without jvm 
+# -r is the option for which script to run, in this case
+# multiple_run_mcd_20 (do not add .m)
+# "< /dev/null" prevents MATLAB giving errors running
+# non-interactively
 
 matlab -nojvm -nodisplay -r multiple_run_mcd_20  < /dev/null
 
 echo "Done with job: $SGE_TASK_ID"
 
+# Note that the .out file will have
+#
+# Warning: no access to tty (Bad file descriptor).
+# Thus no job control in this shell.
+#
+# at top - this can be ignored safely.
