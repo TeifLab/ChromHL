43c33c167fe313ab07eb69de37780a3400e15dd0
Graeme Thorn <graeme.thorn@gmail.com>
2018-05-20 19:23:27 +0100

    Tidy up and add comments

--- compare_peaks_recalc.R
+++ compare_peaks_recalc.R
@@ -1,14 +1,15 @@
-#
-# Script for computing peak size of model and compare with data
-#
-#
-#
-#
+# This R script computes the peak size of the model and compares it with the
+# data
 
-# Read in data from bed file
+# BED file has each region and the position of each peak within
+# that region
 
 peaks<-read.table("HET_6387_centred_20100_full.bed",header=F,as.is=T)
 
+# This file has the following structure:
+#
+# Adjust this for whichever BED file you use
+#
 # col 1-3 extended intervals
 # col 4 name
 # col 5 ""
@@ -16,58 +17,71 @@ peaks<-read.table("HET_6387_centred_20100_full.bed",header=F,as.is=T)
 # col 7-9 peak positions
 # col 10 same as col 4
 # col 11,12 ""
-# col 13,14
 
-peaks$V13 <- NA # will hold peaks size
+# Add two columns to hold the model peak size
+peaks$V13 <- NA
 peaks$V14 <- NA
 
+# Set column names for the output file, based on the BED file
 colnames(peaks) <- c("Chromosome","Left.ext","Right.ext","Name",".",".","Chromosome","Left.peak","Right.peak","Name",".",".","Left.model","Right.model")
 
-peaks_threshold <- 0.5 # simple classifier takes the threshold to be 0.5
+# Set a threshold for setting which p(heterochromatin) means a peak
+peaks_threshold <- 0.5 
 
+# Set the lattice size of the computation and the number of lattice units
 lattice_size <- 161
 no_nucs <- floor((ceiling(40201/lattice_size))/2)*2+1
 reg_centre <-ceiling(no_nucs/2)
 
+# Go through each peak one by one
 for(i in 1:dim(peaks)[1]){
+  # set the filename based on the input region
   filename<-paste0(peaks[i,1],".",peaks[i,2],"-",peaks[i,3],".txt");
-  
   print(filename)
   
-  x <- try(
-    
-    model <- read.table(filename,header=T,as.is=T) 
-    
+  # Try reading in the file
+  x <- try(  
+    model <- read.table(filename,header=T,as.is=T)    
   )
   
-  if(class(x)!="try-error"){
-    
+  if(class(x)!="try-error"){ 
+    # if we do have an output file
+    # it has the following structure:
     # col 1 is site no
     # col 2 is HP1 binding
     # col 3 is State 1 (Heterochromatin) prob
     # col 4 is State 2 (Euchromatin) prob
     # col 5 is CTCF binding site
     
+    # Read outwards from the centre of the region
+    # to get the peak size in terms of lattice
+    # units 
     centre<-reg_centre
     
     left <- NA  
     right <- NA
-    if(model[centre,3]>peaks_threshold){
-      
+    
+    if(model[centre,3]>peaks_threshold){ 
+      # if a heterchromatin region exists at the centre
       left<-reg_centre
       right<-reg_centre
       
+      # start a search in either direction to get the maximum size of the region
       while(model[left,3]>peaks_threshold){
         left <- left -1
-        if(left==0) break;
+        if(left==0) break; # we have reached the end of the region
       }
       while(model[right,3]>peaks_threshold){
         right <- right +1
-        if(right>length(model[,2])) break;
+        if(right>length(model[,2])) break; # we have reached the end of the region
       }
       
-    } # if peak found left is nuc to left, right is nuc to right so need to add/subtract 1
+    } 
     
+    # If a peak is found left is unit to left, right is unit to right so need to add/subtract 1
+    
+    # add to data file
+    # note that the output is in lattice units, not in bp - so MUST be scaled later
     peaks[i,12:13]<-c(left+1,right-1)
     
   }
@@ -75,6 +89,5 @@ for(i in 1:dim(peaks)[1]){
   
 }
 
+# write file to text file, which can be further processed
 write.table(peaks,file="Peaks_file_recalc.txt",sep="\t",quote=F,row.names=F,col.names=T)
-
-
