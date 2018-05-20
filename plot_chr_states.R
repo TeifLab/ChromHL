c318084720f7e1b4a4d4717b275447d6ffd15a69
Graeme Thorn <graeme.thorn@gmail.com>
2018-05-20 19:28:26 +0100

    Update plot_chr_states.R

--- plot_chr_states.R
+++ plot_chr_states.R
@@ -1,3 +1,6 @@
+# Basic output plotting script which reads in each txt output file and
+# generates a PNG file of chromatin states per text file
+
 in.files <- dir(pattern="^chr.*txt$")
  
 for(i in in.files){
@@ -14,7 +17,7 @@ for(i in in.files){
 	
 	png(plot.name,height=300,width=700)
 	
-	matplot(x[,1]-107,x[,3:5],xlab="Nuc pos",ylab="Chromatin state",col=c("black","red","blue"),type="l",lty=1)
+	matplot(x[,1]-107,x[,3:5],xlab="Lattice unit",ylab="Chromatin state",col=c("black","red","blue"),type="l",lty=1)
 	legend("topright",lty=1,col=c("black","red","blue"),legend=c("Het","Eu","CTCF"))
 	
 	dev.off()
